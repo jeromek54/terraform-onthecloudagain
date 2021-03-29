df00bbcb27dbedb149f8bbaaff878e81c65344c8
jeromek54 <81587456+jeromek54@users.noreply.github.com>
2021-03-29 21:15:56 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    aws = {
+      source    = "hashicorp/aws"
+      version   = "~> 3.20"
+    }
+  }
+}
+
+provider "aws" {
+  alias         = "aws01"
+  region        = "eu-west-3"
+}
+provider "aws" {
+  alias         = "aws02"
+  region        = "eu-west-3"
+}
+
+resource "aws_instance" "terra_ec2" {
+  provider = aws.aws02
+
+  ami                           = "ami-0ba7c4110ca9bfe0b"
+  instance_type                 = "t2.micro"
+  key_name                      = var.my-key
+  tags                          = {Test=true}
+
+  ebs_block_device {
+    delete_on_termination       = true
+    device_name                 = "/dev/sdb"
+    encrypted                   = true
+    volume_size                 = 3
+  }
+}
+
+output "instance" {
+  value = aws_instance.terra_ec2.id
+  description = "Sensitive thing!"
+  sensitive = true
+}
