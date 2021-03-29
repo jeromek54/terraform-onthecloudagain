cbb36c193e1f8cbbf1621d68673e7c6b84b34759
jeromek54 <81587456+jeromek54@users.noreply.github.com>
2021-03-29 23:23:32 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,6 +23,7 @@ resource "aws_instance" "terra_ec2" {
   ami                           = "ami-0ba7c4110ca9bfe0b"
   instance_type                 = "t2.micro"
   key_name                      = var.my-key
+  tags                          = {Test="true"}
 
   ebs_block_device {
     delete_on_termination       = true
